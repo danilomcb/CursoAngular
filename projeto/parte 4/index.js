da4d62ab9d251c941620694124cb0ab808daaf48
Rafael Teles <rmteles@gmail.com>
2015-10-14 20:43:38 -0300

    Finalizada parte 4

---
+++ projeto/parte 4/index.js
@@ -0,0 +1,34 @@
+var app = angular.module("financas", [
+    'angular-ladda',
+    'jcs-autoValidate'
+]);
+
+app.config(function ($httpProvider, laddaProvider) {
+    $httpProvider.defaults.headers.common.Authorization = "Tartagura Anonima";
+    laddaProvider.setOption({
+        style: 'expand-right'
+    });
+});
+
+app.controller("Transacoes", ["$scope", "TransacaoService", function ($scope, TransacaoService) {
+    $scope.transacoes = TransacaoService;
+    $scope.transacoes.carregarTransacoes();
+}]);
+
+
+app.factory("TransacaoService", ["$http", function ($http) {
+    var self = {
+        lista: [],
+        selecionada: null,
+        carregarTransacoes: function () {
+            $http.get("http://localhost:8080/curso/transacoes", {}).then(
+                function successCallback(response) {
+                    self.lista = response.data.list;
+                }, function errorCallback(response) {
+                    console.log(response);
+                });
+        }
+    };
+
+    return self;
+}]);
