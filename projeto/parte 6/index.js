2ef82c888b768839464ef3468e59cb9068b76718
Rafael Teles <rmteles@gmail.com>
2015-10-14 22:46:46 -0300

    Primeira etapa da parte 6 feita

---
+++ projeto/parte 6/index.js
@@ -0,0 +1,55 @@
+var app = angular.module("financas", [
+    'angular-ladda',
+    'jcs-autoValidate',
+    'angularSpinner'
+]);
+
+app.config(function ($httpProvider, laddaProvider) {
+    $httpProvider.defaults.headers.common.Authorization = "Tartagura Anonima";
+    laddaProvider.setOption({
+        style: 'expand-right'
+    });
+});
+
+app.controller("TransacoesLista", ["$scope", "TransacaoService", function ($scope, TransacaoService) {
+    $scope.pesquisa = {};
+    $scope.ordem = "nome";
+    $scope.transacoes = TransacaoService;
+    $scope.transacoes.carregarTransacoes();
+
+    $scope.filtrar = function (transacao) {
+        if ($scope.pesquisa.nome || $scope.pesquisa.valor || $scope.pesquisa.geral) {
+            return transacao.nome.indexOf($scope.pesquisa.nome) > 0 ||
+                transacao.nome.indexOf($scope.pesquisa.geral) > 0 ||
+                transacao.valor == $scope.pesquisa.valor ||
+                transacao.valor == $scope.pesquisa.geral;
+        }
+
+        return true;
+    };
+}]);
+
+app.controller("TransacoesDetalhes", ["$scope", "TransacaoService", function ($scope, TransacaoService) {
+    $scope.transacoes = TransacaoService;
+}]);
+
+app.factory("TransacaoService", ["$http", function ($http) {
+    var self = {
+        lista: [],
+        carregando: false,
+        selecionada: null,
+        carregarTransacoes: function () {
+            self.carregando = true;
+            $http.get("http://localhost:8080/curso/transacoes", {}).then(
+                function successCallback(response) {
+                    self.lista = response.data.list;
+                    self.carregando = false;
+                }, function errorCallback(response) {
+                    console.log(response);
+                    self.carregando = false;
+                });
+        }
+    };
+
+    return self;
+}]);
