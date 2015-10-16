d30c664160e96cb472a2bd6a363c44a394dc0115
Rafael Teles <rmteles@gmail.com>
2015-10-16 14:36:09 -0300

    Finalizado parte 6

--- projeto/parte 6/index.js
+++ projeto/parte 6/index.js
@@ -1,10 +1,18 @@
 var app = angular.module("financas", [
     'angular-ladda',
     'jcs-autoValidate',
+    'ngResource',
     'angularSpinner'
 ]);
 
 app.config(function ($httpProvider, laddaProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+
+    //Remove the header containing XMLHttpRequest used to identify ajax call
+    //that would prevent CORS from working
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
     $httpProvider.defaults.headers.common.Authorization = "Tartagura Anonima";
     laddaProvider.setOption({
         style: 'expand-right'
@@ -18,10 +26,8 @@ app.controller("TransacoesLista", ["$scope", "TransacaoService", function ($scop
     $scope.transacoes.carregarTransacoes();
 
     $scope.filtrar = function (transacao) {
-        if ($scope.pesquisa.nome || $scope.pesquisa.valor || $scope.pesquisa.geral) {
-            return transacao.nome.indexOf($scope.pesquisa.nome) > 0 ||
-                transacao.nome.indexOf($scope.pesquisa.geral) > 0 ||
-                transacao.valor == $scope.pesquisa.valor ||
+        if ($scope.pesquisa.geral) {
+            return transacao.nome.indexOf($scope.pesquisa.geral) > 0 ||
                 transacao.valor == $scope.pesquisa.geral;
         }
 
@@ -31,23 +37,84 @@ app.controller("TransacoesLista", ["$scope", "TransacaoService", function ($scop
 
 app.controller("TransacoesDetalhes", ["$scope", "TransacaoService", function ($scope, TransacaoService) {
     $scope.transacoes = TransacaoService;
+
+    $scope.salvar = function () {
+        $scope.transacoes.salvar($scope.transacoes.selecionada)
+    };
+
+    $scope.remover = function () {
+        $scope.transacoes.remover($scope.transacoes.selecionada)
+    };
 }]);
 
-app.factory("TransacaoService", ["$http", function ($http) {
+
+app.factory("Transacao", ["$resource", function ($resource) {
+    return $resource('http://localhost:8080/curso/transacoes/:id', {id: "@id"}, {
+        update: {
+            method: "PUT"
+        }
+    });
+}]);
+
+
+app.factory("TransacaoService", ["Transacao", "$http", function (Transacao, $http) {
     var self = {
         lista: [],
-        carregando: false,
+        isSalvando: false,
+        isDeletando: false,
+        isCarregando: false,
         selecionada: null,
         carregarTransacoes: function () {
-            self.carregando = true;
-            $http.get("http://localhost:8080/curso/transacoes", {}).then(
-                function successCallback(response) {
-                    self.lista = response.data.list;
-                    self.carregando = false;
-                }, function errorCallback(response) {
+            self.isCarregando = true;
+
+            Transacao.get({},
+                function (response) {
+                    self.lista = [];
+                    angular.forEach(response.list, function (transacao) {
+                        self.lista.push(new Transacao(transacao));
+                    });
+                    self.isCarregando = false;
+                },
+                function (error) {
+                    console.log(error);
+                }
+            );
+        },
+        remover: function (transacao) {
+            self.isDeletando = true;
+            transacao.$remove().then(function () {
+                self.isDeletando = false;
+
+                var index = self.lista.indexOf(transacao);
+                self.lista.splice(index, 1);
+                self.selecionada = null;
+            }, function (error) {
+                self.isDeletando = false;
+                console.log(error);
+            });
+        },
+        salvar: function (transacao) {
+            self.isSalvando = true;
+            //Metodo Alternativo
+            //transacao.$update().then(
+            //    function (response) {
+            //        console.log(response);
+            //        self.isSalvando = false;
+            //    }, function (error) {
+            //        console.log(error);
+            //        self.isSalvando = false;
+            //    }
+            //);
+
+            Transacao.update(transacao).$promise.then(
+                function(response) {
                     console.log(response);
-                    self.carregando = false;
-                });
+                    self.isSalvando = false;
+                }, function (error) {
+                    console.log(error);
+                    self.isSalvando = false;
+                }
+            );
         }
     };
 
